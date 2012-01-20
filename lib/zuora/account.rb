062e0d5397475488ba18182e780540a5761d1ba4
Andy Fleener <anfleene@gmail.com>
2012-01-20 13:36:34 -0600

    Added bill to and sold to contacts to Account

--- lib/zuora/account.rb
+++ lib/zuora/account.rb
@@ -1,4 +1,11 @@
 module Zuora
   class Account < ZObject
+    def billTo
+      @billTo ||= Contact.find(self.billToId)
+    end
+
+    def soldTo
+      @soldTo ||= Contact.find(self.soldToId)
+    end
   end
 end
