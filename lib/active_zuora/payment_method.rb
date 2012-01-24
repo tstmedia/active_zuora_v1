6c0eb36a3d8632c566fcc97c36968a4cf46bdfca
Andy Fleener <anfleene@gmail.com>
2012-01-24 13:21:46 -0600

    Ignore ach/cc fields that aren't queryable on payment method

--- lib/active_zuora/payment_method.rb
+++ lib/active_zuora/payment_method.rb
@@ -1,4 +1,7 @@
 module Zuora
   class PaymentMethod < ZObject
+    def self.excluded_attributes(attributes=[])
+      super([:achAccountNumber, :creditCardNumber, :creditCardSecurityCode])
+    end
   end
 end
