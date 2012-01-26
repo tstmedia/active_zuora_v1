89c23a1ef509403ea2c12d72bd423f94a424de5e
Andy Fleener <anfleene@gmail.com>
2012-01-26 13:53:04 -0600

    Fix for api limitation

--- lib/active_zuora/rate_plan_charge.rb
+++ lib/active_zuora/rate_plan_charge.rb
@@ -1,4 +1,7 @@
 module Zuora
   class RatePlanCharge < ZObject
+    def self.excluded_attributes(attributes=[])
+      super([:overagePrice, :includedUnits, :discountAmount, :discountPercentage])
+    end
   end
 end
