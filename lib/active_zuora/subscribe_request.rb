988daabb2c0cab3059d944ecf4f529bf22618111
Andy Fleener <anfleene@gmail.com>
2012-02-21 16:26:50 -0600

    Changed subscribe request to use subscribe method for create and save

--- lib/active_zuora/subscribe_request.rb
+++ lib/active_zuora/subscribe_request.rb
@@ -1,4 +1,13 @@
 module Zuora
   class SubscribeRequest < ZObject
+    def self.create(args={})
+      self.client.subscribe([self.new(args).to_zobject])
+    end
+
+    def save
+      result = self.class.client.subscribe([self.to_zobject])
+      self.account.id = result.accountId
+      result.success
+    end
   end
 end
