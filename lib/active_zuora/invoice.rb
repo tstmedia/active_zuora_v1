f091fba709d18fcc8e707d28d3d9b1b48587eedc
Andy Fleener <anfleene@gmail.com>
2012-02-21 09:45:36 -0600

    Added payments to az payment object

--- lib/active_zuora/invoice.rb
+++ lib/active_zuora/invoice.rb
@@ -11,5 +11,9 @@ module Zuora
     def account
       @account ||= Account.find(self.accountId)
     end
+
+    def payments
+      @invoice_payments ||= InvoicePayment.where(:invoiceid => self.id)
+    end
   end
 end
